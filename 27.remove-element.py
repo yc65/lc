a765f7379ef6150a854bf00d286fa87e20a4a667
Yc65 <xuyichen65@outlook.com>
2019-03-14 21:41:33 +0800

    27 remove elements - two pointers

---
+++ 27.remove-element.py
@@ -0,0 +1,91 @@
+#
+# @lc app=leetcode id=27 lang=python3
+#
+# [27] Remove Element
+#
+# https://leetcode.com/problems/remove-element/description/
+#
+# algorithms
+# Easy (43.61%)
+# Total Accepted:    378.5K
+# Total Submissions: 865.5K
+# Testcase Example:  '[3,2,2,3]\n3'
+#
+# Given an array nums and a value val, remove all instances of that value
+# in-place and return the new length.
+# 
+# Do not allocate extra space for another array, you must do this by modifying
+# the input array in-place with O(1) extra memory.
+# 
+# The order of elements can be changed. It doesn't matter what you leave beyond
+# the new length.
+# 
+# Example 1:
+# 
+# 
+# Given nums = [3,2,2,3], val = 3,
+# 
+# Your function should return length = 2, with the first two elements of nums
+# being 2.
+# 
+# It doesn't matter what you leave beyond the returned length.
+# 
+# 
+# Example 2:
+# 
+# 
+# Given nums = [0,1,2,2,3,0,4,2], val = 2,
+# 
+# Your function should return length = 5, with the first five elements of nums
+# containing 0, 1, 3, 0, and 4.
+# 
+# Note that the order of those five elements can be arbitrary.
+# 
+# It doesn't matter what values are set beyond the returned length.
+# 
+# Clarification:
+# 
+# Confused why the returned value is an integer but your answer is an array?
+# 
+# Note that the input array is passed in by reference, which means modification
+# to the input array will be known to the caller as well.
+# 
+# Internally you can think of this:
+# 
+# 
+# // nums is passed in by reference. (i.e., without making a copy)
+# int len = removeElement(nums, val);
+# 
+# // any modification to nums in your function would be known by the caller.
+# // using the length returned by your function, it prints the first len
+# elements.
+# for (int i = 0; i < len; i++) {
+# print(nums[i]);
+# }
+# 
+#
+class Solution:
+    # solution1 slow-fast two pointer
+    # def removeElement(self, nums: List[int], val: int) -> int:
+    #     len_nums = len(nums)
+    #     if len_nums == 0: return 0
+    #     i = j = 0
+    #     for j in range(len_nums):
+    #         if nums[j] != val:
+    #             nums[i] = nums[j]
+    #             i += 1
+    #     return i
+    # solution2 two pointers - wh
+    # en elements to be removed are rare
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i, j = 0, len(nums)
+        while i < j:
+            if nums[i] == val:
+                nums[i] = nums[j-1]
+                j -= 1
+            else:
+                i+= 1
+        return j
+
+
+
