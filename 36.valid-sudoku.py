785f3cf42384107a3b66a7d1bcaf896b766c16b6
Yc65 <xuyichen65@outlook.com>
2019-03-16 21:32:24 +0800

    36 valid sudoku

---
+++ 36.valid-sudoku.py
@@ -0,0 +1,104 @@
+#
+# @lc app=leetcode id=36 lang=python3
+#
+# [36] Valid Sudoku
+#
+# https://leetcode.com/problems/valid-sudoku/description/
+#
+# algorithms
+# Medium (41.88%)
+# Total Accepted:    218.9K
+# Total Submissions: 520.2K
+# Testcase Example:  '[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]'
+#
+# Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be
+# validated according to the following rules:
+# 
+# 
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without
+# repetition.
+# 
+# 
+# 
+# A partially filled sudoku which is valid.
+# 
+# The Sudoku board could be partially filled, where empty cells are filled with
+# the character '.'.
+# 
+# Example 1:
+# 
+# 
+# Input:
+# [
+# ⁠ ["5","3",".",".","7",".",".",".","."],
+# ⁠ ["6",".",".","1","9","5",".",".","."],
+# ⁠ [".","9","8",".",".",".",".","6","."],
+# ⁠ ["8",".",".",".","6",".",".",".","3"],
+# ⁠ ["4",".",".","8",".","3",".",".","1"],
+# ⁠ ["7",".",".",".","2",".",".",".","6"],
+# ⁠ [".","6",".",".",".",".","2","8","."],
+# ⁠ [".",".",".","4","1","9",".",".","5"],
+# ⁠ [".",".",".",".","8",".",".","7","9"]
+# ]
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input:
+# [
+# ["8","3",".",".","7",".",".",".","."],
+# ["6",".",".","1","9","5",".",".","."],
+# [".","9","8",".",".",".",".","6","."],
+# ["8",".",".",".","6",".",".",".","3"],
+# ["4",".",".","8",".","3",".",".","1"],
+# ["7",".",".",".","2",".",".",".","6"],
+# [".","6",".",".",".",".","2","8","."],
+# [".",".",".","4","1","9",".",".","5"],
+# [".",".",".",".","8",".",".","7","9"]
+# ]
+# Output: false
+# Explanation: Same as Example 1, except with the 5 in the top left corner
+# being 
+# ⁠   modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is
+# invalid.
+# 
+# 
+# Note:
+# 
+# 
+# A Sudoku board (partially filled) could be valid but is not necessarily
+# solvable.
+# Only the filled cells need to be validated according to the mentioned
+# rules.
+# The given board contain only digits 1-9 and the character '.'.
+# The given board size is always 9x9.
+# 
+# 
+#
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        from collections import defaultdict
+        # row_set and col_set of 9
+        row_set = defaultdict(set)
+        col_set = defaultdict(set)
+        # matrx_set of 3, reset every three row
+        matrx_set = defaultdict(set)
+        # with the three sets, we only need to loop the board once
+        for r in range(0, 9):
+            if r % 3 == 0:
+                matrx_set.clear()
+            for c in range(0, 9):
+                matrix_id = c//3
+                curr = board[r][c]
+                if curr != '.':
+                    if curr in row_set[r] or curr in col_set[c] or curr in matrx_set[matrix_id]:
+                        return False
+                    row_set[r].add(curr)
+                    col_set[c].add(curr)
+                    matrx_set[matrix_id].add(curr)
+        return True
+
