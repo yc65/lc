b4cd99c3676084f57ad4e631444199d37f74edf5
Yc65 <xuyichen65@outlook.com>
2019-03-24 18:16:32 +0800

    58 length of last word

---
+++ 58.length-of-last-word.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=58 lang=python3
+#
+# [58] Length of Last Word
+#
+# https://leetcode.com/problems/length-of-last-word/description/
+#
+# algorithms
+# Easy (32.18%)
+# Total Accepted:    252.2K
+# Total Submissions: 783.7K
+# Testcase Example:  '"Hello World"'
+#
+# Given a string s consists of upper/lower-case alphabets and empty space
+# characters ' ', return the length of last word in the string.
+# 
+# If the last word does not exist, return 0.
+# 
+# Note: A word is defined as a character sequence consists of non-space
+# characters only.
+# 
+# Example:
+# 
+# Input: "Hello World"
+# Output: 5
+# 
+# 
+#
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        if not s: return 0
+        return len(s.strip().split(' ')[-1])
+
+
