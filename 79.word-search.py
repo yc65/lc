c3807cdafe6a32cb6be80b941db1e0ff9bedb9be
Yc65 <xuyichen65@outlook.com>
2019-03-29 11:02:33 +0800

    79 word search - backtracking

---
+++ 79.word-search.py
@@ -0,0 +1,88 @@
+#
+# @lc app=leetcode id=79 lang=python3
+#
+# [79] Word Search
+#
+# https://leetcode.com/problems/word-search/description/
+#
+# algorithms
+# Medium (30.58%)
+# Total Accepted:    261.1K
+# Total Submissions: 851.9K
+# Testcase Example:  '[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]\n"ABCCED"'
+#
+# Given a 2D board and a word, find if the word exists in the grid.
+# 
+# The word can be constructed from letters of sequentially adjacent cell, where
+# "adjacent" cells are those horizontally or vertically neighboring. The same
+# letter cell may not be used more than once.
+# 
+# Example:
+# 
+# 
+# board =
+# [
+# ⁠ ['A','B','C','E'],
+# ⁠ ['S','F','C','S'],
+# ⁠ ['A','D','E','E']
+# ]
+# 
+# Given word = "ABCCED", return true.
+# Given word = "SEE", return true.
+# Given word = "ABCB", return false.
+# 
+# 
+#
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        if not word: return True
+        m = len(board)
+        if m == 0:
+            return False
+        n = len(board[0])
+        if n == 0:
+            return False
+
+        def backtracking(x, y, w2match, b):
+            if not w2match:
+                return True
+
+            # NOTE: validate the indices in the beginning of a loop
+            if x<0 or y<0 or x>m-1 or y>n-1 or b[x][y] != w2match[0]:
+                return False
+
+            # NOTE: to prevent from reusing the char
+            temp = b[x][y]
+            b[x][y] = "#"
+
+            res = backtracking(x-1, y, w2match[1:], b) or \
+                backtracking(x, y-1, w2match[1:], b) or \
+                backtracking(x+1, y, w2match[1:], b) or \
+                backtracking(x, y+1, w2match[1:], b)
+
+            # NOTE: restore the original board
+            b[x][y] = temp
+            return res
+
+            # if x>0 and b[x-1][y] == w2match[0]:
+            #     res = backtracking(x-1, y, w2match[1:], b)
+            #     if res == True: out=res
+            # if y>0 and b[x][y-1] == w2match[0]: 
+            #     res = backtracking(x, y-1, w2match[1:], b)
+            #     if res == True: out=res
+            # if x<m-1 and b[x+1][y] == w2match[0]:
+            #     res = backtracking(x+1, y, w2match[1:], b)
+            #     if res == True: out=res
+            # if y<n-1 and b[x][y+1] == w2match[0]:
+            #     res = backtracking(x, y+1, w2match[1:], b)
+            #     if res == True: out=res
+
+            # return out
+
+        for i in range(m):
+            for j in range(n):
+                if backtracking(i, j, word,board):
+                    return True
+
+        return False
+
