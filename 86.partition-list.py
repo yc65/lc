6ffb0ca85cae603e93091ee3fb44f95190b09b9d
Yc65 <xuyichen65@outlook.com>
2019-03-30 22:37:23 +0800

    86 partition list - two pointers

---
+++ 86.partition-list.py
@@ -0,0 +1,51 @@
+#
+# @lc app=leetcode id=86 lang=python3
+#
+# [86] Partition List
+#
+# https://leetcode.com/problems/partition-list/description/
+#
+# algorithms
+# Medium (36.51%)
+# Total Accepted:    156.7K
+# Total Submissions: 427.7K
+# Testcase Example:  '[1,4,3,2,5,2]\n3'
+#
+# Given a linked list and a value x, partition it such that all nodes less than
+# x come before nodes greater than or equal to x.
+# 
+# You should preserve the original relative order of the nodes in each of the
+# two partitions.
+# 
+# Example:
+# 
+# 
+# Input: head = 1->4->3->2->5->2, x = 3
+# Output: 1->2->2->4->3->5
+# 
+# 
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        small_head = small = ListNode(0)
+        large_head=large = ListNode(0)
+
+        while head:
+            if head.val < x:
+                small.next = head
+                small = small.next
+            else:
+                large.next = head
+                large = large.next
+            head = head.next
+        # don't forget to set the last one's next as None
+        large.next = None
+        small.next = large_head.next
+        return small_head.next
+
