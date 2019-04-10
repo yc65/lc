71bfb30550f26ef43426509f5d986a49d7834b29
yc65 <41785318+yc65@users.noreply.github.com>
2019-04-10 09:34:30 +0800

    Update 7.reverse-integer.py

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -42,6 +42,47 @@
 # 
 #
 class Solution:
-    def reverse(self, x: int) -> int:
+    def myAtoi(self, str: str) -> int:
+        max_int = 2**31-1
+        min_int = -2**31
+        positive = True
+        str_len = len(str)
+        # char_to_digit = {"1":1, "2":2, "3":3, "4":4, "5":5, "6":6,"7":7, "8":8,"9":9, "0":0}
+        idx = 0
+        res = 0
+        status = -1 # -1: unknown status; 1: appending digits
+
+        while idx < str_len:
+            c = str[idx]
+            # temp = char_to_digit.get(c)
+            temp = ord(c)-ord("0") # use this instead of dictionary to speedup
+            if status == -1:
+                if c == "-": 
+                    positive = False
+                    status = 1
+                elif c == "+":
+                    status = 1
+                # elif temp != None:
+                elif temp >=0 and temp<=9:
+                    status = 1
+                    res = res * 10 + temp
+                elif c != " ":
+                    return 0
+            elif status == 1: 
+                if temp >= 0 and temp<=9:
+                    res = res*10+temp
+                else:
+                    break
+            idx += 1
+        if positive == True:
+            if res > max_int:
+                return max_int
+            else:
+                return res
+        if positive == False:
+            if 0-res < min_int:
+                return min_int
+            else:
+                return 0-res
         
 
