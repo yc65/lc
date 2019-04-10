1c23181f7bcc9373e90ebefdab1e376199589d3d
yc65 <41785318+yc65@users.noreply.github.com>
2019-04-10 09:37:01 +0800

    Update 7.reverse-integer.py

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -42,47 +42,21 @@
 # 
 #
 class Solution:
-    def myAtoi(self, str: str) -> int:
-        max_int = 2**31-1
-        min_int = -2**31
-        positive = True
-        str_len = len(str)
-        # char_to_digit = {"1":1, "2":2, "3":3, "4":4, "5":5, "6":6,"7":7, "8":8,"9":9, "0":0}
-        idx = 0
-        res = 0
-        status = -1 # -1: unknown status; 1: appending digits
-
-        while idx < str_len:
-            c = str[idx]
-            # temp = char_to_digit.get(c)
-            temp = ord(c)-ord("0") # use this instead of dictionary to speedup
-            if status == -1:
-                if c == "-": 
-                    positive = False
-                    status = 1
-                elif c == "+":
-                    status = 1
-                # elif temp != None:
-                elif temp >=0 and temp<=9:
-                    status = 1
-                    res = res * 10 + temp
-                elif c != " ":
-                    return 0
-            elif status == 1: 
-                if temp >= 0 and temp<=9:
-                    res = res*10+temp
-                else:
-                    break
-            idx += 1
-        if positive == True:
-            if res > max_int:
-                return max_int
-            else:
-                return res
-        if positive == False:
-            if 0-res < min_int:
-                return min_int
-            else:
-                return 0-res
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        res=0
+        negative=False
+        if x<0:
+            negative=True
+            x=abs(x)
+        res=int(str(x)[::-1])
+        if res>2**31:
+            res=0 
+        elif negative:
+            res=0-res
+        return res
         
 
