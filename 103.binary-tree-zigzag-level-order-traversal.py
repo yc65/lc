86cab5a9c19dfcdcc27d042316cb0337396652c9
Yc65 <xuyichen65@outlook.com>
2019-04-13 16:30:57 +0800

    103. binary tree zigzag level order traversal

---
+++ 103.binary-tree-zigzag-level-order-traversal.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode id=103 lang=python3
+#
+# [103] Binary Tree Zigzag Level Order Traversal
+#
+# https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+#
+# algorithms
+# Medium (40.73%)
+# Total Accepted:    207.4K
+# Total Submissions: 504.3K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# Given a binary tree, return the zigzag level order traversal of its nodes'
+# values. (ie, from left to right, then right to left for the next level and
+# alternate between).
+# 
+# 
+# For example:
+# Given binary tree [3,9,20,null,null,15,7],
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+# 
+# 
+# return its zigzag level order traversal as:
+# 
+# [
+# ⁠ [3],
+# ⁠ [20,9],
+# ⁠ [15,7]
+# ]
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        res = []
+        def dfs(root, level):
+            if not root:
+                return
+            if len(res) <= level:
+                res.append([])
+            if level % 2 == 1:
+                res[level].insert(0, root.val)
+            else:
+                res[level].append(root.val)
+            dfs(root.left, level+1)
+            dfs(root.right, level+1)                
+        dfs(root, 0)
+        return res
+
