efb2688a81022703964fdc4b23e1e08ffd421f2f
Yc65 <xuyichen65@outlook.com>
2019-04-14 12:36:53 +0800

    110. balanced binary tree

---
+++ 110.balanced-binary-tree.py
@@ -0,0 +1,77 @@
+#
+# @lc app=leetcode id=110 lang=python3
+#
+# [110] Balanced Binary Tree
+#
+# https://leetcode.com/problems/balanced-binary-tree/description/
+#
+# algorithms
+# Easy (40.49%)
+# Total Accepted:    308.1K
+# Total Submissions: 757.5K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# Given a binary tree, determine if it is height-balanced.
+# 
+# For this problem, a height-balanced binary tree is defined as:
+# 
+# 
+# a binary tree in which the depth of the two subtrees of every node never
+# differ by more than 1.
+# 
+# 
+# Example 1:
+# 
+# Given the following tree [3,9,20,null,null,15,7]:
+# 
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+# Return true.
+# 
+# Example 2:
+# 
+# Given the following tree [1,2,2,3,3,null,null,4,4]:
+# 
+# 
+# ⁠      1
+# ⁠     / \
+# ⁠    2   2
+# ⁠   / \
+# ⁠  3   3
+# ⁠ / \
+# ⁠4   4
+# 
+# 
+# Return false.
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        def recur(node):
+            # print(node.val)
+            if not node:
+                return 0
+            l= recur(node.left)
+            r = recur(node.right)
+            if l == -1 or r == -1:
+                return -1
+            elif abs(l-r) > 1:
+                return -1 # NOTE: use -1 to mark unbalanced 
+                          # and to pass unbalance to the next call
+            else:
+                return max(l, r)+1
+        return recur(root) != -1
+
+        
