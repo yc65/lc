230efd17e95a7671f6ca9ad662e450d07f21bbd3
yc65 <41785318+yc65@users.noreply.github.com>
2019-04-17 17:55:32 +0800

    124. binary tree maximum path sum

---
+++ 124.binary-tree-maximum-path-sum.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode id=124 lang=python3
+#
+# [124] Binary Tree Maximum Path Sum
+#
+# https://leetcode.com/problems/binary-tree-maximum-path-sum/description/
+#
+# algorithms
+# Hard (29.68%)
+# Total Accepted:    182.5K
+# Total Submissions: 614.6K
+# Testcase Example:  '[1,2,3]'
+#
+# Given a non-empty binary tree, find the maximum path sum.
+# 
+# For this problem, a path is defined as any sequence of nodes from some
+# starting node to any node in the tree along the parent-child connections. The
+# path must contain at least one node and does not need to go through the
+# root.
+# 
+# Example 1:
+# 
+# 
+# Input: [1,2,3]
+# 
+# ⁠      1
+# ⁠     / \
+# ⁠    2   3
+# 
+# Output: 6
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: [-10,9,20,null,null,15,7]
+# 
+# -10
+# / \
+# 9  20
+# /  \
+# 15   7
+# 
+# Output: 42
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+        res = float('-inf')
+        def recur(node, res):
+            if not node:
+                return 0
+            l = recur(node.left, res)
+            r = recur(node.right, res)
+            curr_max = max([l+node.val, r+node.val, node.val, l+r+node.val])
+            res = max(res, curr_max)
+            return res
+        res = recur(root, res)
+        return res
