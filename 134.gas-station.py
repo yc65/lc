1776eaaaa97570a11cd85f19f1dd4d6205b26fc6
Yc65 <xuyichen65@outlook.com>
2019-04-21 21:50:53 +0800

    134.gas station

--- 134.gas-station.py
+++ 134.gas-station.py
@@ -75,15 +75,23 @@
 #
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # 1. 假如从位置i开始，i+1，i+2...，一路开过来一路油箱都没有空。说明什么？说明从i到i+1，i+2，...肯定是正积累。
+        # 2. 现在突然发现开往位置j时油箱空了。这说明什么？
+        # 说明从位置i开始没法走完全程(废话)。
+        # 那么，我们要从位置i+1开始重新尝试吗？
+        # 不需要！为什么？因为前面已经知道，位置i肯定是正积累，
+        # 那么，如果从位置i+1开始走更加没法走完全程了，
+        # 因为没有位置i的正积累了。同理，也不用从i+2，i+3，...开始尝试。
+        # 所以我们可以放心地从位置j+1开始尝试。
         len_gas = len(gas)
         start = 0
         rest = 0
         overall = 0
         # if sum(gas) < sum(cost), then we cannot find an answer
         for i in range(len_gas):
-            rest = gas[i]-cost[i]
+            rest += gas[i]-cost[i]
             overall += gas[i]-cost[i]
             if rest < 0: # if rest < 0, we can't start at this point
-                rest, start = 0, start+1
+                rest, start = 0, i+1
         return start if overall>=0 else -1
 
