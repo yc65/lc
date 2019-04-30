fd478e4744d4169be16cf6f98e7abe696f8294c3
Yc65 <xuyichen65@outlook.com>
2019-04-30 22:15:40 +0800

    152.maximum product subarray - dp - faster

--- 152.maximum-product-subarray.py
+++ 152.maximum-product-subarray.py
@@ -11,10 +11,13 @@ class Solution:
         res = min_val = max_val = nums[0]
         for n in nums[1:]:
             # when multiplied by a negative number, the minvalue becomes the maxvalue
-            # and the maxvalue becomes the minvalue
-            if n < 0:
-                min_val, max_val = max_val, min_val
-            min_val = min(n, min_val*n)
-            max_val = max(n, max_val*n)
+            # and the maxvalue becomes the minval
+            if n<0:
+                temp = min_val # don't forget to use temp here
+                min_val = min(n, max_val*n)
+                max_val = max(n, temp*n)
+            else:
+                min_val = min(n, min_val*n)
+                max_val = max(n, max_val*n)
             res = max(max_val, res)
         return res
