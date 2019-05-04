8e760f9ffafb7356a012af445349c9a848ddc6fe
Yc65 <xuyichen65@outlook.com>
2019-05-04 23:28:36 +0800

    168.excel sheet column title - maths

---
+++ 168.excel-sheet-column-title.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=168 lang=python3
+#
+# [168] Excel Sheet Column Title
+#
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        capitals = [chr(x) for x in range(ord('A'), ord('Z')+1)]
+        res = ""
+        while n > 0:
+            remainder = n % 26
+            res = capitals[remainder-1] + res
+            n = (n-1) // 26
+        # res += capitals[n-1] + res
+        return res
